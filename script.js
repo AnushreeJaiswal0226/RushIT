d35eb739582fa8826c53b657de8e163ba788a1a4
Anushree Jaiswal <91840205+AnushreeJaiswal0226@users.noreply.github.com>
2022-12-14 02:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.nav-wrapper');
+
+/*window.addEventListener('scroll', () => {
+    if(window.scrollY >= 50){
+        navbar.classList.add('bgcolor');
+    }else{
+        navbar.classList.remove('bgcolor');
+    }
+});*/
